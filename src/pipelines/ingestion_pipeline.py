b8bb6bb830c42bb40ae4c851b8e881bda1797096
Pooja Jain <pooja.ashok.jain@accenture.com>
2026-03-02 11:33:47 +0530

    Create ingestion Pipeline

--- src/pipelines/ingestion_pipeline.py
+++ src/pipelines/ingestion_pipeline.py
@@ -1,3 +1,13 @@
+import subprocess
+import sys
+
+subprocess.check_call(
+    [sys.executable, "-m", "pip", "install",
+     "boto3", "pandas", "sqlalchemy==1.4.46", "pymysql", "pytz"]
+)
+
+
+
 import argparse
 import json
 import boto3
